f45a5d9e2a7240dfc89132ca0593ab55f79e957d
dzon4xx <janpleszynski@gmail.com>
2016-07-18 19:10:46 +0200

    Solved issues with relations

--- common/relations/dependancy.py
+++ common/relations/dependancy.py
@@ -6,7 +6,7 @@ from common.elements.clock import clock
 from common.elements.input_element import Input_element
 from common.elements.output_element import Output_element
 
-from common.sys_types import efect_status
+from common.sys_types import effect_status
 
 get_millis = lambda: int(round(time.time() * 1000))
 
@@ -39,7 +39,7 @@ class Condition():
         """Representation of object"""
         return "".join(["ID: ",str(self.id),"\teval: ", self.evaluate()])
 
-class Efect():
+class Effect():
 
     def __init__(self, id, el_id, value, time):
         self.id = id
@@ -48,7 +48,7 @@ class Efect():
         self.time = self.__parse_time(time)
         self.priority = 1
 
-        self.done = False # flaga mowiaca o tym czy wystartowac dany efekt. Zmieniana na True po kazdym pozytywnym wyliczeniu przyczyny. Jesli efekt wykonany to przechodzi do stanu False
+        self.done = True # flaga mowiaca o tym czy wystartowac dany efekt. Zmieniana na True po kazdym pozytywnym wyliczeniu przyczyny. Jesli efekt wykonany to przechodzi do stanu False
         self.cause_time = None # Moment uruchomienia przyczyny
 
     def __parse_time(self, time):
@@ -80,10 +80,10 @@ class Dependancy():
 
     cond_start = '[' #marker poczatku warunku
     cond_stop = ']'
-    efect_start_t = '{'
-    efect_stop_t = '}'
+    effect_start_t = '{'
+    effect_stop_t = '}'
     cond_marker = '!' # marker przeczyny wstawiany w stringa przyczyn. Potem w to miejsce wstawiana jest wartosc wyrazenia przyczyny (True, False)
-    cause_efect_sep = 'then' #separator pomiedzy przyczynami a efektami
+    cause_effect_sep = 'then' #separator pomiedzy przyczynami a efektami
     time_marker = 't'
     day_marker = 'd'
     element_marker = 'e'
@@ -104,17 +104,18 @@ class Dependancy():
         self.name = name
         self.dep_str = dep_str
         self.conditions = [] #lista warunkow aby przyczyna zaleznosci byla spelniona
-        self.efects = []     #efekty, ktore wydarza sie po spelnieniu przyczyny
+        self.effects = []     #efekty, ktore wydarza sie po spelnieniu przyczyny
 
         self.num_of_conds = 0
-        self.num_of_efects = 0
-        self.num_of_done_efects = 0
+        self.num_of_effect = 0
+        self.num_of_done_effect = 0
 
+        self.prev_result = False
 
-        cause_str, efect_str = dep_str.split(Dependancy.cause_efect_sep)
+        cause_str, effect_str = dep_str.split(Dependancy.cause_effect_sep)
         self.cause_template = "" # szablon przyczyny do ktorego wstrzykiwane sa wartosci warunkow.
         self.__parse_cause(cause_str)
-        self.__parse_efects(efect_str)
+        self.__parse_effect(effect_str)
        
     def __parse_cause(self, cause_str):
         condition_num = 0
@@ -179,40 +180,40 @@ class Dependancy():
         subscribe(condition)
         self.conditions.append(condition)
 
-    def __parse_efects(self, efect_str):
-        efect_str = efect_str.strip().rstrip(';')
-        efects = efect_str.split(';')
-        for efect in efects:
-            efect = efect.strip()
+    def __parse_effect(self, effect_str):
+        effect_str = effect_str.strip().rstrip(';')
+        effect_array = effect_str.split(';')
+        for effect in effect_array:
+            effect = effect.strip()
             op_pos = 0
             time_pos = None
             time = ''
             is_time = False
-            for s_pos, s in enumerate(efect):
+            for s_pos, s in enumerate(effect):
                 if s == '=':
                     op_pos = s_pos
                    
-                if s == Dependancy.efect_start_t:
+                if s == Dependancy.effect_start_t:
                     time_pos = s_pos
                     is_time = True 
 
                 if is_time:
                     time += s
 
-                if s == Dependancy.efect_stop_t:
+                if s == Dependancy.effect_stop_t:
                     is_time = False 
 
-            element_id = int(efect[1:op_pos])
+            element_id = int(effect[1:op_pos])
 
             if element_id not in Output_element.items.keys():
                 raise Dependancy_config_error('Output element: ' + str(element_id) + ' not in output elements')
             
             time = time[1:-1] # pierwszy i ostatni znak to markery poczatku i konca czasu
-            set_value = int(efect[op_pos+1:time_pos])
+            set_value = int(effect[op_pos+1:time_pos])
             
-            efect = Efect(self.num_of_efects, element_id, set_value, time)
-            self.num_of_efects += 1
-            self.efects.append(efect)
+            effect = Effect(self.num_of_effect, element_id, set_value, time)
+            self.num_of_effect += 1
+            self.effects.append(effect)
         
     def evaluate_cause(self, ):
         eval_causes = ''
@@ -223,13 +224,13 @@ class Dependancy():
             else:
                 eval_causes += s
 
-        self.evaluate_cause.prev_result = False
+        
         result = eval(eval_causes)
 
-        if result and  not self.evaluate_cause.prev_result:
-            self.evaluate_cause.prev_result = result
-            for efect in efects:
-                efect.notify(get_millis())
+        if result and not self.prev_result: # notyfikacja tylko gdy przyczyna zmieni sie z false z false na true
+            self.prev_result = result
+            for effect in self.effects:
+                effect.notify(get_millis())
 
         return result
 
