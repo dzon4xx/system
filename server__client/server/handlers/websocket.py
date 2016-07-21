3926dfeeba401a92a2b56964d81d738c8c3f8e4e
dzon4xx <janpleszynski@gmail.com>
2016-07-21 21:01:24 +0200

    handling logic not connected to server

--- server__client/server/handlers/websocket.py
+++ server__client/server/handlers/websocket.py
@@ -25,18 +25,26 @@ class Websocket(WebSocketHandler):
 
     def on_message(self, message):
         socket_logger.debug('%s send: %s',self.name, message)
+
         if self == Websocket.logic:
             data = message.split(',')
             Visual_element.items[int(data[0][1:])].value = data[1]
             for con in Websocket.clients:
-                socket_logger.debug('passing')
+                socket_logger.debug('passing to clients')
                 con.write_message(message)
         else:
+            socket_logger.debug('sending to logic')
             Websocket.logic.write_message(message)
 
     def on_close(self):
         if self != Websocket.logic:        
+            socket_logger.info("client disconected")
             Websocket.clients.remove(self)
+        if self == Websocket.logic:
+            socket_logger.info("logic disconected")
+            Websocket.logic = None
+
+
 
     def check_origin(self, origin):
         return True
