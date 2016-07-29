6341c9d31e664fa250f00f60532bc83c426eb460
dzon4xx <janpleszynski@gmail.com>
2016-07-29 10:25:03 +0200

    1) modbus variable name changed to modbus_manager.
    It matches class name

--- backend/start.py
+++ backend/start.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import logging
 #import ptvsd
 #tcp://backend@localhost:8082
 #ptvsd.enable_attach(secret="backend", address = ('127.0.0.1', 8082))
@@ -11,29 +12,32 @@ from common.color_logs import color_logs
 
 #zabezpieczyc bufory przed kolizja watkow. Tzn logika usuwa bufor a w tym samym czasie komunikacja pisze do buforu
 color_logs()
-modbus = Modbus_manager()
 communication = Communication_manager()
 logic = Logic_manager(args=(communication,))
-modbus.tasks = logic.tasks
+modbus_manager = Modbus_manager()
+modbus_manager.tasks = logic.tasks
 
 
 
 communication.logger.disabled = False
-communication.logger.setLevel("DEBUG")
+communication.logger.setLevel("INFO")
 
-modbus.logger.disabled = False
-modbus.logger.setLevel("DEBUG")
+modbus_manager.logger.disabled = False
+modbus_manager.logger.setLevel("DEBUG")
+
+modbus_manager.modbus.logger.disabled = False
+modbus_manager.modbus.logger.setLevel("INFO")
 
 logic.logger.disabled = False
-logic.logger.setLevel("DEBUG")
+logic.logger.setLevel("INFO")
 
 communication.setDaemon(True)
 logic.setDaemon(True)
-modbus.setDaemon(True)
+modbus_manager.setDaemon(True)
 
 communication.start()
 logic.start()
-modbus.start()
+modbus_manager.start()
 
 while threading.active_count() > 0:
     time.sleep(2)
