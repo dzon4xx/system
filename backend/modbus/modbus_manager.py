6a0998c310dd5e377c2457bad6d83b5380871b99
dzon4xx <janpleszynski@gmail.com>
2016-07-29 10:25:03 +0200

    1) Support for refactorized code of common elements. 2) Tasks are now just output modules to be notified

--- backend/modbus/modbus_manager.py
+++ backend/modbus/modbus_manager.py
@@ -4,13 +4,10 @@ import time
 from serial import SerialException
 
 from common.sys_types import mt, et, task_stat
-from common.elements.input_element import Input_element
-from common.elements.output_element import Output_element
-from common.elements.element import Element
 
-from common.modules.input_module import Input_module
-from common.modules.output_module import Output_module
-from common.modules.module import Module
+from common.elements.element import Element, Output_element, Input_element
+
+from common.modules.module import Module, Input_module, Output_module, Anfa_output, Anfa_led_light, Anfa_ambient, Anfa_input
 
 from common.benchmark import Benchmark
 
@@ -26,45 +23,41 @@ class Modbus_manager(threading.Thread):
         self.logger = logging.getLogger('MODBUS_MAN')
 
         self.modbus = Modbus(250000)
-        self.modbus.logger.disabled = False
+
         self.tasks = None # tasks are instantated by start script
 
         self._setup()
 
     def _setup(self, ):
         
-        self._db.load_objects_from_table(Input_module)
-        self._db.load_objects_from_table(Output_module)
+        self._db.load_objects_from_table(Anfa_output)
+        self._db.load_objects_from_table(Anfa_led_light)
+        self._db.load_objects_from_table(Anfa_ambient)
+        self._db.load_objects_from_table(Anfa_input)
 
         for module in Module.items.values():
             module.modbus = self.modbus #pass modbus reference to every module
 
-        for input_element in Input_element.items.values():
-            Input_module.items[input_element.module_id].regs[input_element.reg_id] = input_element
-
-        #for output_element in Output_element.items.values():
-        #    Output_module.items[output_element.module_id].regs[input_element.reg_id] = input_element
+        for element in Element.items.values():
+            module = Module.items[element.module_id]
+            module.elements[element.reg_id] = element # pass elements to modules registers
 
     def _check_tasks(self, ):
 
-        output_module_to_write = dict() # dictionary for performing modbus write nad notifying output elements
-        while not self.tasks.empty():
-            output_element = self.tasks.get() # get output element form task queue
-            output_module = Output_module.items[output_element.module_id]
-            output_module.regs_values[output_element.reg_id]
+        not_done_tasks = set()
+        while self.tasks:
+            output_module = self.tasks.pop() # get output element form task queue
+            output_module.write()
 
-            if output_module not in output_module_to_write.keys():
-                output_module_to_write[output_module] = []
-                output_module_to_write[output_module].append(output_element)
-            else:
-                output_module_to_write[output_module].append(output_element)
-
-        for output_module, output_elements in output_module_to_write.items():
-            result = output_module.write()
-            if result:
-                for output_element in output_elements:
-                    output_element.new_val_flag = True
-                    output_element.value = output_element.desired_value
+        #    if output_module.is_available():
+        #        result = output_module.write()
+        #        if result == False:
+        #            output_module.timeout = 3
+        #            not_done_tasks.add(output_module) # put module to task queue
+        #    else:
+        #        not_done_tasks.add(output_module)
+        #while not_done_tasks:
+        #    self.tasks.add(not_done_tasks.pop())
 
     def run(self, ):
         if (self.modbus.connected):
@@ -76,18 +69,15 @@ class Modbus_manager(threading.Thread):
                     self._check_tasks() #after every read of in_mod check if there is anything to write to out_mod
                     if input_module.is_available():
                         input_module.read() # reds values and sets them to elements
-                    else: 
-                        input_module.set_timeout(3) # after [n] seconds try to establish communication with module 
+                        pass
                 for input_element in Input_element.items.values():
                     if input_element.prev_value != input_element.value:
                         input_element.new_val_flag = True
-                        input_element.prev_value = input_element.value
                         self.logger.debug('New val: {}'.format(str(input_element)))
-
+                        input_element.prev_value = input_element.value
 
                 lps = bench.loops_per_second()
-                if lps:
-                    self.logger.debug(lps)
+
         else:
             self.logger.error('Modbus connection error. Thread {} exits'.format(self.name))
 
