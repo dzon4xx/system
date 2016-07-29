8dde597aed2a2023c43e1505825cffe4df6cc008
dzon4xx <janpleszynski@gmail.com>
2016-07-29 10:25:03 +0200

    1) All classes are now in one file.
    It is easier to maintain them as they are'nt very long
    It is better to look on one file than skip through 5 files
    
    2) Blinds now support other blind feature. Two blind motors will never turn on in the same time
    
    Signed-off-by: dzon4xx <janpleszynski@gmail.com>

--- common/elements/element.py
+++ common/elements/element.py
@@ -1,5 +1,6 @@
 from common.base_object import Base_object
 from common.sys_types import et
+from math import floor
 
 class Element(Base_object):
 
@@ -7,9 +8,7 @@ class Element(Base_object):
     COL_MODULE_ID, COL_REG,  = 3, 4, #numery kolumn
     column_headers_and_types = Base_object.column_headers_and_types + [['module_id', 'integer'], ['register', 'integer']] 
 
-    ID = 0
     items = {}
-
     def __init__(self, *args):
         super().__init__(args[0], et(args[1]), args[2]) # inicjalizuj id type, name
         Element.items[self.id] = self        
@@ -29,11 +28,87 @@ class Element(Base_object):
     def __str__(self, ):
         return  "".join( [super().__str__(), "\tvalue: ", str(self.value)])
 
+class Input_element(Element):
+
+    types = set((et.ds, et.dht_hum, et.dht_temp, et.pir, et.rs, et.ls, et.switch))
+    items = {}
+
+    def __init__(self, *args):
+        """arguments: type name """
+        super().__init__(*args) # inicjalizuj type, name
+        Input_element.items[self.id] = self     
+        self.prev_value = None
+        self.unit = None  
+
+    def __str__(self, ):
+        return  "".join([super().__str__(), "\tprev val: ", str(self.prev_value), ])
+
+class Output_element(Element):
+ 
+    types = set((et.led, et.heater, et.ventilator, et.blind))
+
+    defualt_priority = 10
+    items = {}
+    def __init__(self, *args):
+        super().__init__(*args)
+        Output_element.items[self.id] = self 
+        self.desired_value = None
+        self.setter_priority = Output_element.defualt_priority   #bardzo niski priorytet. kazy moze go zmienic
+
+    def set_desired_value(self, priority, val):
+        if val > 0:
+            if priority <= self.setter_priority:
+                self.setter_priority = priority
+                self.desired_value = val
+                return True
+        elif val == 0:# przy wylaczeniu kazdy moze ustawic element
+            self.setter_priority = Output_element.defualt_priority
+            self.desired_value = 0 
+            return False
+
+
+    def __str__(self, ):
+        return  "".join([super().__str__(), "\tdesired_value: ", str(self.desired_value)])#str(self.desired_value), "\tmodule id: ", str(self.module_id), "\tport id: ", str(self.reg_id)])
+
+    def elements_str():
+        string = "\n"
+        for element in Output_element.items.values():
+            el_str = str(element) 
+            string += el_str + "\n"
+
+        return string
+
+class Blind(Output_element):
+    table_name = "blinds"
+    column_headers_and_types = Element.column_headers_and_types + [['direction', 'text'], ['other_blind', 'integer']] 
+
+    COL_DIRECTION, COL_OTHER_BLIND = 5, 6
+    items = {}
+    def __init__(self, *args):
+        super().__init__(*args[0:self.COL_DIRECTION])
+        Blind.items[self.id] = self
+        self.direction = args[self.COL_DIRECTION]
+        self.other_blind = args[self.COL_OTHER_BLIND] # if blind is up then it is down and the other way around
+
+    def set_desired_value(self, priority, val):
+        if super(Blind, self).set_desired_value(priority, 1): # if setter has right priority to turn on blind
+            self.other_blind.desired_value = 0    # other blind motor is turned off as well ot avoid shortcut
+            return True
+        return False
+
+
 if __name__ == "__main__":
-    from common.sys_types import et
-    for i in range(4):
-        el = Element(et.ds, 'element')
-        print (str(el))
+    pass
+
+from common.sys_types import et
+types = set((et.ds, et.dht_hum, et.dht_temp, et.pir, et.rs, et.ls, et.switch))
+num_types = set()
+
+while types:
+    num_types.add(types.pop().value)
+
+num_types
+
 
 
 
