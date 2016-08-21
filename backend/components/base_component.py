cf4f65e3f018d860930981e28a3eae70d0e57630
dzon4xx <janpleszynski@gmail.com>
2016-08-21 13:07:56 +0200

    Added arguments check

--- backend/components/base_component.py
+++ backend/components/base_component.py
@@ -1,20 +1,34 @@
-class Base_component(object):
-   """Base component for all system components It stores id type name and trivial string representation"""
-   column_headers_and_types = [['id', 'integer primary key'], 
+from enum import Enum
+
+
+class BaseComponent(object):
+    """Base component for all system components It stores id type name and trivial string representation"""
+    column_headers_and_types = [['id', 'integer primary key'],
                                 ['type', 'integer'],
-                                ['name', 'text'],]
-   COL_ID, COL_TYPE, COL_NAME = 0, 1, 2 #numery kolumn
+                                ['name', 'text'], ]
+    COL_ID, COL_TYPE, COL_NAME = 0, 1, 2  # numery kolumn
 
-
-   def __init__(self, *args):
+    def __init__(self, *args):
         self.id, self.type, self.name = args
 
-   def __str__(self, ):
-        """Representation of object"""
-        return "".join(["ID: ",str(self.id),"\ttype: ", self.type.name, "\tname: ", self.name])
+        #if not isinstance(self.id, int):
+        #   raise TypeError("Id must be int")
 
-   def __repr__(self,):
-       return "".join(["ID: ",str(self.id)," type: ", self.type.name])
+        if not (isinstance(self.type, int) or isinstance(self.type, Enum)):
+            raise TypeError("Type must be int or enum")
+
+        if not isinstance(self.name, str):
+            raise TypeError("name must be str")
+
+        #if self.id < 0:
+        #    raise ValueError("Component id must be positive number")
+
+    def __str__(self, ):
+        """Representation of object"""
+        return "".join(["ID: ", str(self.id), "\ttype: ", self.type.name, "\tname: ", self.name])
+
+    def __repr__(self,):
+        return "".join(["ID: ", str(self.id), " type: ", self.type.name])
 
 
 
